58778b4917f5ed2902073a2bf36f50aeb0ad6ede
sumaiya ahmed <sumu0612@gmail.com>
2026-02-07 16:25:50 +0600

    course schema updated

--- src/modules/order/order.service.ts
+++ src/modules/order/order.service.ts
@@ -119,8 +119,8 @@ const createPayment = async (req: Request) => {
       };
     });
   });
-  if(!createOrder.id){
-    throw new Error("Failed to create order");
+  if (!createOrder.id) {
+    throw new Error('Failed to create order');
   }
   if (!createOrder.id) {
     throw new Error('Failed to create order');
@@ -138,11 +138,12 @@ const createPayment = async (req: Request) => {
       total_amount: amount,
       currency: 'BDT',
       tran_id: orderId,
-      success_url: `${config.FRONTEND_URL}/payment/success`,
+      success_url: `${config.FRONTEND_URL}/`,
       fail_url: `${config.FRONTEND_URL}/payment/fail`,
       cancel_url: `${config.FRONTEND_URL}/payment/cancel`,
+      ipn_url: `${config.IPN_VALIDATION_URL}`,
       // ipn_url: `https://aloskill-backend-production.up.railway.app/api/v1/order/validate-ipn`,
-      ipn_url: `https://fortunate-kindness-production.up.railway.app/api/v1/order/validate-ipn`,
+      // ipn_url: `https://fortunate-kindness-production.up.railway.app/api/v1/order/validate-ipn`,
       ...(paymentMethod ? { multi_card_name: paymentMethod } : {}),
       cus_email: userEmail,
       cus_phone: userPhone,
